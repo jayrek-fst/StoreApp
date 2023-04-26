bbbaf91b33a7c636d4d68433035140ce5b5d4591
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-04-26 15:27:37 +0800

    add font family

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store_app/util/constant_util.dart';
-// import 'package:store_app/cubit/product_list/product_cubit.dart';
-
-import 'bloc/product/product_bloc.dart';
 import 'repository/product_repository.dart';
 import 'package:http/http.dart' as http;
 
@@ -22,9 +19,7 @@ class StoreApp extends StatelessWidget {
       create: (context) => ProductRepository(client: http.Client()),
       child: MaterialApp(
         title: ConstantUtil.appName,
-        theme: ThemeData(
-          primarySwatch: Colors.red,
-        ),
+        theme: ThemeData(primarySwatch: Colors.red, fontFamily: 'Poppins'),
         onGenerateRoute: RouteGenerator.generateRoute,
         initialRoute: ConstantUtil.routeDashboard,
       ),
